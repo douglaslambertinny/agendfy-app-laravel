fecd63807e49891b054c1a65f99afaed8bbe9dce
Douglas Barbosa <douglas.barbosa@indigoway.com>
2024-08-10 12:30:44 -0300

    project with tenancy

---
+++ README.md
@@ -0,0 +1,11 @@
+# Agendfy with laravel
+
+## Para executar o projeto
+
+```$ docker compose up -d```
+
+```$ docker compose exec app composer install```
+
+```$ docker compose exec app artisan migrate```
+
+```$ docker compose exec app npm install```
--- web/agendfy/routes/api.php
+++ web/agendfy/routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// foreach (config('tenancy.central_domains') as $domain) {
+//     Route::domain($domain)->group(function () {
+//         // your actual routes
+//     });
+// }
